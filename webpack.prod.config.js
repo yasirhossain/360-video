d8fb4f1e991716eda44ef8981ae10dd484f048b3
Yasir Hossain <yasir.hossain@theplatform.com>
2016-12-28 14:36:27 -0800

    added bundled js file

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, 'dist/public'),
-    filename: 'threesixty.js',
+    filename: 'bundle.js',
     libraryTarget: 'var',
     library: 'ThreeSixty',
     publicPath: '/dist/public/'
