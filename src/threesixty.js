7dba1ad1f7a632c6e635bdcb5637f59ace518303
Yasir Hossain <yasir.hossain@theplatform.com>
2016-12-29 12:37:10 -0800

    moved detector call inside

--- src/threesixty.js
+++ src/threesixty.js
@@ -1,51 +1,7 @@
 // TODO strip out options
-// TODO strip out video data source and add video html element to grab src
+// TODO see if you can remove need for window / document by using attach to pass elements only
 
-const Detector = {
-  canvas: !! window.CanvasRenderingContext2D,
-  webgl: ( function () { try { var canvas = document.createElement( 'canvas' ); return !! window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ); } catch( e ) { return false; } } )(),
-  workers: !! window.Worker,
-  fileapi: window.File && window.FileReader && window.FileList && window.Blob,
-  getWebGLErrorMessage: () => {
-    let element = document.createElement('div')
-    element.id = 'webgl-error-message'
-    element.style.fontFamily = 'monospace'
-    element.style.fontSize = '13px'
-    element.style.fontWeight = 'normal'
-    element.style.textAlign = 'center'
-    element.style.background = '#fff'
-    element.style.color = '#000'
-    element.style.padding = '1.5em'
-    element.style.width = '800px'
-    element.style.margin = '5em auto 0'
-    if (!self.webgl) {
-      element.innerHTML = window.WebGLRenderingContext ? [
-        'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
-        'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-      ].join( '\n' ) : [
-        'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
-        'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-      ].join( '\n' );
-    }
-
-    return element;
-  },
-  addGetWebGLMessage: (parameters) => {
-    let parent, id, element
-
-    parameters = parameters || {}
-
-    parent = parameters.parent !== undefined ? parameters.parent : document.body
-    id = parameters.id !== undefined ? parameters.id : 'oldie'
-
-    element = Detector.getWebGLErrorMessage()
-    element.id = id
-
-    parent.appendChild(element)
-  }
-}
-
-const ThreeSixty = (THREE, Detector, window, document, undefined) => {
+const ThreeSixty = (THREE, window, document) => {
   let self = {},
       defaults = {
         crossOrigin: 'anonymous',
@@ -64,6 +20,50 @@ const ThreeSixty = (THREE, Detector, window, document, undefined) => {
       },
       div,
       video
+
+    const Detector = {
+      canvas: !! window.CanvasRenderingContext2D,
+      webgl: ( function () { try { var canvas = document.createElement( 'canvas' ); return !! window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ); } catch( e ) { return false; } } )(),
+      workers: !! window.Worker,
+      fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+      getWebGLErrorMessage: () => {
+        let element = document.createElement('div')
+        element.id = 'webgl-error-message'
+        element.style.fontFamily = 'monospace'
+        element.style.fontSize = '13px'
+        element.style.fontWeight = 'normal'
+        element.style.textAlign = 'center'
+        element.style.background = '#fff'
+        element.style.color = '#000'
+        element.style.padding = '1.5em'
+        element.style.width = '800px'
+        element.style.margin = '5em auto 0'
+        if (!self.webgl) {
+          element.innerHTML = window.WebGLRenderingContext ? [
+            'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+            'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+          ].join( '\n' ) : [
+            'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+            'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+          ].join( '\n' );
+        }
+
+        return element;
+      },
+      addGetWebGLMessage: (parameters) => {
+        let parent, id, element
+
+        parameters = parameters || {}
+
+        parent = parameters.parent !== undefined ? parameters.parent : document.body
+        id = parameters.id !== undefined ? parameters.id : 'oldie'
+
+        element = Detector.getWebGLErrorMessage()
+        element.id = id
+
+        parent.appendChild(element)
+      }
+    }
 
   const attachContainer = (element) => {
     self.element = element
